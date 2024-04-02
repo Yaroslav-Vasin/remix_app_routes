03f0e27cfce763c27590505ab68380db0bc001c6
Yaroslav-Vasin <yaroslav.vasin@bystrov.agency>
2024-04-02 15:31:12 +0300

    first

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+// tailwind.config.js
+
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} as Config;
